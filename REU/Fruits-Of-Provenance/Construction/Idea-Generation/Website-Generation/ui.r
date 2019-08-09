59bcc1578fae87967c0160081172cb0487b5de8e
eoduniyi <eeoduniyi@gmail.com>
2019-08-09 09:09:05 -0400

    Added REU work
    The collection of notes, ideas, and prototypes related to provBookR

---
+++ REU/Fruits-Of-Provenance/Construction/Idea-Generation/Website-Generation/ui.r
@@ -0,0 +1,12 @@
+# Define UI for miles per gallon app ----
+ui <- pageWithSidebar(
+  
+  # App title ----
+  headerPanel("Miles Per Gallon"),
+  
+  # Sidebar panel for inputs ----
+  sidebarPanel(),
+  
+  # Main panel for displaying outputs ----
+  mainPanel()
+)
